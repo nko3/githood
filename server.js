32985063c120c821aed533b20baa584bdcaa2e0a
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2012-11-10 15:32:00 -0500

    extract config to separate file

--- server.js
+++ server.js
@@ -17,5 +17,17 @@ http.createServer(privateApp).listen(privateApp.port, '127.0.0.1', function() {
   console.log("Private Express server listening on http://127.0.0.1:" + privateApp.port);
 });
 
+
+//var gitHttp = new GitHttp();
+//gitHttp.on('pull', function(pull) { pull.allow(); });
+//gitHttp.on('push', function(pull) { pull.deny(); });
+
+//http.createServer(function(req, res) {
+  //gitHttp.handle(req, res);
+//}).listen(8002);
+
+var config = require('./lib/config').get();
+console.log(config);
+
 var browser = new browser(privateApp.neighbors);
 browser.start();
