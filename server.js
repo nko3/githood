7141af7734d96023c8f5e8156856f62cd921ebce
David Singley <dsingley@nearinfinity.com>
2012-11-10 12:31:19 -0500

    move browsing to a sepearate file

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var http = require('http');
 var mdns = require('mdns');
 var sf = require("sf");
 var request = require('request');
-
 var publicApp = require('./lib/publicApp');
 var privateApp = require('./lib/privateApp');
+var browser = require('./lib/browser');
 
 http.createServer(publicApp).listen(publicApp.port, function() {
   var txt_record = { name: 'Githood', description: publicApp.config.server.description };
@@ -12,45 +12,10 @@ http.createServer(publicApp).listen(publicApp.port, function() {
   ad.start();
   console.log("Public Express server listening on http://0.0.0.0:" + publicApp.port);
 });
+
 http.createServer(privateApp).listen(privateApp.port, '127.0.0.1', function() {
   console.log("Private Express server listening on http://127.0.0.1:" + privateApp.port);
 });
 
-var browser = mdns.createBrowser(mdns.tcp('http'));
-
-browser.on('serviceUp', function(service) {
-  if (service.txtRecord && service.txtRecord.name === 'Githood') {
-    var n = {
-             name: service.name,
-             description: service.txtRecord.description,
-             host: service.host,
-             port: service.port,
-             repositories: []
-            };
-    var url = sf("http://{host}:{port}/apiv1/repos", n);
-
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(url, 'SUCCESS:', body);
-      } else {
-        console.log(url, 'ERROR:', error);
-      }
-    })
-
-    privateApp.neighbors.push(n);
-    console.log("service up:", n);
-  }
-});
-browser.on('serviceDown', function(service) {
-  privateApp.neighbors = privateApp.neighbors.filter(function(n) {
-    if (n.name === service.name) {
-      console.log("service down:", n);
-      return false;
-    }
-    else {
-      return true;
-    }
-  });
-});
-
+var browser = new browser(privateApp.neighbors);
 browser.start();
