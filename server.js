fa0101628d77e0bd382f96c84d92f34f3252c45b
Evan Oxfeld <eoxfeld@nearinfinity.com>
2012-11-10 02:41:17 -0500

    Create sample views for both webapps

--- server.js
+++ server.js
@@ -1,57 +1,18 @@
-var express = require('express');
-var routes = require('./lib/routes');
-var user = require('./lib/routes/user');
 var http = require('http');
-var path = require('path');
 var fs = require('fs');
 var mdns = require('mdns');
 
-var publicApp = express();
-var privateApp = express();
+var publicApp = require('./lib/publicApp');
+var privateApp = require('./lib/privateApp');
 
-publicApp.configure(function(){
-  publicApp.set('config', process.env.CONFIG || process.env.HOME + '/.githood');
-  publicApp.set('port', process.env.PUBLIC_PORT || 8000);
-  publicApp.set('views', __dirname + '/lib/views');
-  publicApp.set('view engine', 'ejs');
-  publicApp.use(express.favicon());
-  publicApp.use(express.logger('dev'));
-  publicApp.use(express.bodyParser());
-  publicApp.use(express.methodOverride());
-  publicApp.use(publicApp.router);
-  publicApp.use(express.static(path.join(__dirname, 'public')));
-});
-privateApp.configure(function(){
-  privateApp.set('config', publicApp.get('config'));
-  privateApp.set('port', process.env.PRIVATE_PORT || 8001);
-  privateApp.set('views', __dirname + '/views');
-  privateApp.set('view engine', 'jade');
-  privateApp.use(express.favicon());
-  privateApp.use(express.logger('dev'));
-  privateApp.use(express.bodyParser());
-  privateApp.use(express.methodOverride());
-  privateApp.use(privateApp.router);
-  privateApp.use(express.static(path.join(__dirname, 'public')));
-});
-
-publicApp.configure('development', function(){
-  publicApp.use(express.errorHandler());
-});
-privateApp.configure('development', function(){
-  privateApp.use(express.errorHandler());
-});
-
-publicApp.get('/', routes.index);
-publicApp.get('/users', user.list);
-
-http.createServer(publicApp).listen(publicApp.get('port'), function(){
+http.createServer(publicApp).listen(publicApp.get('port'), function() {
   var config = JSON.parse(fs.readFileSync(publicApp.get('config')));
   var txt_record = { name: 'Githood', description: config.server.description };
   var ad = mdns.createAdvertisement(mdns.tcp('http'), publicApp.get('port'), {txtRecord: txt_record});
   ad.start();
   console.log("Public Express server listening on http://0.0.0.0:" + publicApp.get('port'));
 });
-http.createServer(privateApp).listen(privateApp.get('port'), '127.0.0.1', function(){
+http.createServer(privateApp).listen(privateApp.get('port'), '127.0.0.1', function() {
   console.log("Private Express server listening on http://127.0.0.1:" + privateApp.get('port'));
 });
 
