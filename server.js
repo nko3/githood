54e157e0ffffd572e38a083ad365004725e38d05
David Singley <dsingley@nearinfinity.com>
2012-11-10 02:39:20 -0500

    keep track of our neighbors

--- server.js
+++ server.js
@@ -54,3 +54,32 @@ http.createServer(publicApp).listen(publicApp.get('port'), function(){
 http.createServer(privateApp).listen(privateApp.get('port'), '127.0.0.1', function(){
   console.log("Private Express server listening on http://127.0.0.1:" + privateApp.get('port'));
 });
+
+var browser = mdns.createBrowser(mdns.tcp('http'));
+var neighbors = [];
+
+browser.on('serviceUp', function(service) {
+  if (service.txtRecord && service.txtRecord.name === 'Githood') {
+    var n = {
+             name: service.name,
+             description: service.txtRecord.description,
+             host: service.host,
+             port: service.port
+            };
+    neighbors.push(n);
+    console.log("service up:", n.name, n.host, n.port);
+  }
+});
+browser.on('serviceDown', function(service) {
+  neighbors = neighbors.filter(function(n) {
+    if (n.name === service.name) {
+      console.log("service down:", n.name, n.host, n.port);
+      return false;
+    }
+    else {
+      return true;
+    }
+  });
+});
+
+browser.start();
