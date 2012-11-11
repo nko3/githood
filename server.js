00c0e10da0f01eb1a75c2031089df2f05a4fd287
David Singley <dsingley@nearinfinity.com>
2012-11-11 16:25:02 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var http = require('http');
-var discovery = require('./lib/discovery');
+var git = require('./lib/git');
 var app = require('./lib/app');
-var gitServer = require('./lib/gitServer');
+var discovery = require('./lib/discovery');
 
 var config = require('./lib/config').get();
 var gitPort = process.env.GIT_PORT || config.server.gitPort || 8000;
 
-http.createServer(gitServer).listen(gitPort, function() {
-  console.log("Public Git server listening on http://0.0.0.0:" + gitPort);
+var gitServer = http.createServer(git);
+gitServer.listen(gitPort, function() {
+  console.log("Public Git server listening on http://" + gitServer.address().address + ":" + gitServer.address().port);
 });
 
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+var appServer = http.createServer(app);
+var io = require('socket.io').listen(appServer);
 
-server.listen(app.port, '127.0.0.1', function() {
-  console.log("Private Express server listening on http://127.0.0.1:" + app.port);
+appServer.listen(app.port, '127.0.0.1', function() {
+  console.log("Private Express server listening on http://" + appServer.address().address + ":" + appServer.address().port);
 });
 
 var sockets = {};
