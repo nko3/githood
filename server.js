19e77c1bd3c2db9bb9e959ea63e651d0708e3664
David Singley <dsingley@nearinfinity.com>
2012-11-10 20:50:04 -0500

    seperate mdns advertiser so it can be restarted

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 var http = require('http');
-var mdns = require('mdns');
 var publicApp = require('./lib/publicApp');
 var privateApp = require('./lib/privateApp');
-var browser = require('./lib/browser');
+var mdnsAdvertiser = require('./lib/mdnsAdvertiser');
+var mdnsBrowser = require('./lib/mdnsBrowser');
 var gitServer = require('./lib/gitServer');
 
+var config = require('./lib/config').get();
+var mdnsAdvertiser;
+
 http.createServer(publicApp).listen(publicApp.port, function() {
-  var txt_record = { name: 'Githood', description: publicApp.config.server.description };
-  var ad = mdns.createAdvertisement(mdns.tcp('http'), publicApp.port, {txtRecord: txt_record});
-  ad.start();
+  mdnsAdvertiser = new mdnsAdvertiser(config.server.description, publicApp.port);
+  mdnsAdvertiser.start();
   console.log("Public Express server listening on http://0.0.0.0:" + publicApp.port);
 });
 
-var config = require('./lib/config').get();
 var publicGitPort = process.env.PUBLIC_GIT_PORT || config.server.publicGitPort || 8001;
 http.createServer(gitServer).listen(publicGitPort, function() {
   console.log("Public Git server listening on http://0.0.0.0:" + publicGitPort);
 });
 
+privateApp.advertiser = mdnsAdvertiser;
 http.createServer(privateApp).listen(privateApp.port, '127.0.0.1', function() {
   console.log("Private Express server listening on http://127.0.0.1:" + privateApp.port);
 });
 
-var browser = new browser(privateApp.neighbors);
-browser.start();
+var mdnsBrowser = new mdnsBrowser(privateApp.neighbors);
+mdnsBrowser.start();
