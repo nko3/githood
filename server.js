f3ca387216d439689e3d2baa4f7d5774f401a2f3
David Singley <dsingley@nearinfinity.com>
2012-11-11 11:10:24 -0500

    cleanup before merge

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var http = require('http');
-var mdns = require('./lib/mdns');
-var publicApp = require('./lib/publicApp');
+var discovery = require('./lib/discovery');
 var privateApp = require('./lib/privateApp');
-var mdnsBrowser = require('./lib/mdnsBrowser');
 var gitServer = require('./lib/gitServer');
-
-//http.createServer(publicApp).listen(publicApp.port, function() {
-  //var txt_record = { name: 'Githood', description: publicApp.config.server.description };
-  //var ad = mdns.createAdvertisement(mdns.tcp('http'), publicApp.port, {txtRecord: txt_record});
-  //privateApp.ad = ad;
-  //ad.start();
-  //console.log("Public Express server listening on http://0.0.0.0:" + publicApp.port);
-//});
 
 var config = require('./lib/config').get();
 var publicGitPort = process.env.PUBLIC_GIT_PORT || config.server.publicGitPort || 8001;
@@ -25,5 +15,5 @@ http.createServer(privateApp).listen(privateApp.port, '127.0.0.1', function() {
 });
 
 config.repos.forEach(function(repo) {
-  mdns.advertiseGitRepo(config.server.description, publicGitPort, repo);
+  discovery.advertiseGitRepo(config.server.description, publicGitPort, repo);
 });
