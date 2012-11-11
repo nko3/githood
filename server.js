14a37bf0531911d83cfd857cadfe84ffc954ad9b
David Singley <dsingley@nearinfinity.com>
2012-11-10 22:12:06 -0500

    adverts working again (but not yet restarting)

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 var http = require('http');
+var mdns = require('mdns');
 var publicApp = require('./lib/publicApp');
 var privateApp = require('./lib/privateApp');
-var mdnsAdvertiser = require('./lib/mdnsAdvertiser');
 var mdnsBrowser = require('./lib/mdnsBrowser');
 var gitServer = require('./lib/gitServer');
 
-var config = require('./lib/config').get();
-var mdnsAdvertiser;
-
 http.createServer(publicApp).listen(publicApp.port, function() {
-  mdnsAdvertiser = new mdnsAdvertiser(config.server.description, publicApp.port);
-  mdnsAdvertiser.start();
+  var txt_record = { name: 'Githood', description: publicApp.config.server.description };
+  var ad = mdns.createAdvertisement(mdns.tcp('http'), publicApp.port, {txtRecord: txt_record});
+  privateApp.ad = ad;
+  ad.start();
   console.log("Public Express server listening on http://0.0.0.0:" + publicApp.port);
 });
 
+var config = require('./lib/config').get();
 var publicGitPort = process.env.PUBLIC_GIT_PORT || config.server.publicGitPort || 8001;
 http.createServer(gitServer).listen(publicGitPort, function() {
   console.log("Public Git server listening on http://0.0.0.0:" + publicGitPort);
 });
 
-privateApp.advertiser = mdnsAdvertiser;
 http.createServer(privateApp).listen(privateApp.port, '127.0.0.1', function() {
   console.log("Private Express server listening on http://127.0.0.1:" + privateApp.port);
 });
