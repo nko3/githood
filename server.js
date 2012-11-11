6699ee7a822e35a12fb05ab48afe15e12a2eaa8f
David Singley <dsingley@nearinfinity.com>
2012-11-11 12:26:33 -0500

    rename app dir

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var http = require('http');
 var discovery = require('./lib/discovery');
-var privateApp = require('./lib/privateApp');
+var app = require('./lib/app');
 var gitServer = require('./lib/gitServer');
 
 var config = require('./lib/config').get();
-var publicGitPort = process.env.PUBLIC_GIT_PORT || config.server.publicGitPort || 8001;
+var gitPort = process.env.GIT_PORT || config.server.gitPort || 8000;
 
-http.createServer(gitServer).listen(publicGitPort, function() {
-  console.log("Public Git server listening on http://0.0.0.0:" + publicGitPort);
+http.createServer(gitServer).listen(gitPort, function() {
+  console.log("Public Git server listening on http://0.0.0.0:" + gitPort);
 });
 
-http.createServer(privateApp).listen(privateApp.port, '127.0.0.1', function() {
-  console.log("Private Express server listening on http://127.0.0.1:" + privateApp.port);
+http.createServer(app).listen(app.port, '127.0.0.1', function() {
+  console.log("Private Express server listening on http://127.0.0.1:" + app.port);
 });
 
 config.repos.forEach(function(repo) {
   if (repo.shared) {
-    discovery.advertiseGitRepo(config.server.description, publicGitPort, repo);
+    discovery.advertiseGitRepo(config.server.description, gitPort, repo);
   }
 });
